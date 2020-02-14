3c8d4862f4eac1f09d7175dbd3f33489ed786045
khuynh4 <kehu0726@colorado.edu>
2020-02-14 01:34:02 -0700

    TicTacToe

---
+++ TicTacToe.h
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <map> 
+
+
+enum class SquareType { Empty, X, O };
+
+struct Position{
+	int row;
+	int col;
+
+	bool operator==(const Position &other) {
+		return row == other.row && col == other.col;
+	}
+	bool operator!=(const Position &other) {
+		return !(row == other.row && col == other.col);
+	}
+
+	Position():row(0),col(0){}
+	Position(const int row_, const int col_):row(row_), col(col_){}
+};
+
+std::string SquareTypeStringify(SquareType sq);
+
+class TicTacToe{
+
+public:
+	TicTacToe();
+
+ 	int get_rows() const {return sizeof(arr_) / sizeof(arr_[0]);}  
+ 	int get_cols() const {return sizeof(arr_[0]) / sizeof(int);}		
+
+ 	SquareType get_square_value(Position pos) const;
+
+	void set_square_value(Position pos, SquareType value);
+
+ 	void create_board();
+
+private:
+  	SquareType arr_[3][3];
+
+	int rows_; // might be convenient but not necessary
+	int cols_;
+};
